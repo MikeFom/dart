1e65e1790c860d46589f7a3bff4796400ab6f947
MikeFom <90593666+MikeFom@users.noreply.github.com>
2021-09-17 20:30:43 +0800

    Add files via upload

---
+++ first_exersice.dart
@@ -0,0 +1,8 @@
+void main() 
+{
+  List list = [60, 999, 14, "dart1", 45, 95, 'dart', 1];
+  final String str = 'dart';
+  final int integer = 15;
+  print(list.contains(str));
+  print(list.contains(integer));
+}
