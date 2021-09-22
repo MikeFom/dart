847cd33f25ab3e0c5f570622accae28495549398
MikeFom <90593666+MikeFom@users.noreply.github.com>
2021-09-22 15:27:09 +0800

    Add files via upload

---
+++ first_exersice.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  List<String> str1 = ["a", "ab", "abc"];
+  List<String> str2 = ["abcde", "ab", "abc"];
+  List<String> str3 = [];
+  print (charCount(str1));
+  print (charCount(str2));
+  print (charCount(str3));
+}
+
+int  charCount (List<String> list)
+{
+  int sum = 0;
+  for (int i = 0; i < list.length; i++)
+    sum+=list[i].length;
+  return sum;
+}
